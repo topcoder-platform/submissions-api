a29dc5025fb2136a6049209f0466acea285208a7
Gunasekar K <gunasekark@Gunasekars-MacBook-Pro.local>
2018-08-16 18:52:38 +0530

    circleci integration

--- build.sh
+++ build.sh
@@ -8,4 +8,5 @@ AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 # Builds Docker image of the app.
 TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
 sed -i='' "s|submissions-api:latest|$TAG|" docker/docker-compose.yml
+echo "" > docker/api.env
 docker-compose -f docker/docker-compose.yml build submissions-api
