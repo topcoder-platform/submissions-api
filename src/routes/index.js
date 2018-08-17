2ec433afb8734d541446be39ee884005032d74c6
Mithun Kamath <callmekatootie@gmail.com>
2018-08-17 16:03:27 +0530

    Add health check

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,5 +7,12 @@ const ReviewTypeRoutes = require('./ReviewTypeRoutes')
 const SubmissionRoutes = require('./SubmissionRoutes')
 const ReviewRoutes = require('./ReviewRoutes')
 const ReviewSummationRoutes = require('./ReviewSummationRoutes')
+const HealthCheck = require('./HealthCheckRoute')
 
-module.exports = _.extend({}, ReviewTypeRoutes, SubmissionRoutes, ReviewRoutes, ReviewSummationRoutes)
+module.exports = _.extend({},
+  ReviewTypeRoutes,
+  SubmissionRoutes,
+  ReviewRoutes,
+  ReviewSummationRoutes,
+  HealthCheck
+)
---
+++ src/services/HealthCheckService.js
@@ -0,0 +1,30 @@
+/**
+ * Health Check Service
+ */
+
+const errors = require('common-errors')
+const helper = require('../common/helper')
+
+/**
+ * Check if the elasticsearch connection is active
+ */
+
+function * check () {
+  const esClient = helper.getEsClient()
+
+  try {
+    yield esClient.ping({
+      requestTimeout: 10000
+    })
+  } catch (e) {
+    throw new errors.HttpStatusError(503, 'Elasticsearch instance cannot be reached')
+  }
+
+  return {
+    checksRun: 1
+  }
+}
+
+module.exports = {
+  check
+}
