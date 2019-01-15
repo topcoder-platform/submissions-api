bca57e821f26ea0426bbd3c7bb11c79a93eda8df
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2019-01-15 16:19:15 +0530

    missed to add auth0 proxy URL in bus api calling.

--- src/common/helper.js
+++ src/common/helper.js
@@ -68,7 +68,7 @@ function getBusApiClient () {
     busApiClient = busApi(_.pick(config,
       ['AUTH0_URL', 'AUTH0_AUDIENCE', 'TOKEN_CACHE_TIME',
         'AUTH0_CLIENT_ID', 'AUTH0_CLIENT_SECRET', 'BUSAPI_URL',
-        'KAFKA_ERROR_TOPIC']))
+        'KAFKA_ERROR_TOPIC', 'AUTH0_PROXY_SERVER_URL']))
   }
 
   return busApiClient
