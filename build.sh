e37af6d1efca6efa61d25fb68f4ae0290097d27d
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2019-03-21 15:14:14 +0530

    changes in circleci yml

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eo pipefail
-ENV=$1
-AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
-AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
-AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY") 
+#ENV=$1
+#AWS_ACCOUNT_ID=$(eval "echo \$${ENV}_AWS_ACCOUNT_ID")
+#AWS_REGION=$(eval "echo \$${ENV}_AWS_REGION")
+#AWS_REPOSITORY=$(eval "echo \$${ENV}_AWS_REPOSITORY")
 
 # Builds Docker image of the app.
-TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
-sed -i='' "s|submissions-api:latest|$TAG|" docker/docker-compose.yml
+#TAG=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$AWS_REPOSITORY:$CIRCLE_BUILD_NUM
+#sed -i='' "s|submissions-api:latest|$TAG|" docker/docker-compose.yml
 echo "" > docker/api.env
 docker-compose -f docker/docker-compose.yml build submissions-api
 docker images
