2410f5742d5fd508082c2c74b648f36867847f42
Dushyant Bhalgami <dushyantbhalgami@gmail.com>
2019-09-16 12:25:17 +0530

    issue fix - value of status is not in existing reviwe as well as in paylod while updating review

--- src/services/ReviewService.js
+++ src/services/ReviewService.js
@@ -2,21 +2,19 @@
  * Review Service
  */
 
-const errors = require('common-errors')
-const _ = require('lodash')
-const uuid = require('uuid/v4')
-const joi = require('joi')
-const logger = require('winston')
+const errors = require('common-errors');
+const _ = require('lodash');
+const uuid = require('uuid/v4');
+const joi = require('joi');
+const logger = require('winston');
 
-const dbhelper = require('../common/dbhelper')
-const helper = require('../common/helper')
-const {
-  originator, mimeType, events
-} = require('../../constants').busApiMeta
-const HelperService = require('./HelperService')
-const SubmissionService = require('./SubmissionService')
+const dbhelper = require('../common/dbhelper');
+const helper = require('../common/helper');
+const { originator, mimeType, events } = require('../../constants').busApiMeta;
+const HelperService = require('./HelperService');
+const SubmissionService = require('./SubmissionService');
 
-const table = 'Review'
+const table = 'Review';
 
 /**
  * Function to get review based on ID from DynamoDB
@@ -24,16 +22,16 @@ const table = 'Review'
  * @param {Number} reviewId reviewId which need to be retrieved
  * @return {Object} Data retrieved from database
  */
-function * _getReview (reviewId) {
+function* _getReview(reviewId) {
   // Construct filter to retrieve record from Database
   const filter = {
     TableName: table,
     Key: {
-      'id': reviewId
+      id: reviewId
     }
-  }
-  const result = yield dbhelper.getRecord(filter)
-  return result.Item
+  };
+  const result = yield dbhelper.getRecord(filter);
+  return result.Item;
 }
 
 /**
@@ -42,44 +40,56 @@ function * _getReview (reviewId) {
  * @param {Number} reviewId reviewId which need to be found
  * @return {Object} Data found from database or ES
  */
-function * getReview (authUser, reviewId) {
-  let review
-  const response = yield helper.fetchFromES({
-    id: reviewId
-  }, helper.camelize(table))
+function* getReview(authUser, reviewId) {
+  let review;
+  const response = yield helper.fetchFromES(
+    {
+      id: reviewId
+    },
+    helper.camelize(table)
+  );
 
   if (response.total === 0) {
-    logger.info(`Couldn't find review ${reviewId} in ES. Checking db`)
-    review = yield _getReview(reviewId)
-    logger.debug(`Review: ${review}`)
+    logger.info(`Couldn't find review ${reviewId} in ES. Checking db`);
+    review = yield _getReview(reviewId);
+    logger.debug(`Review: ${review}`);
 
     if (!review) {
-      throw new errors.HttpStatusError(404, `Review with ID = ${reviewId} is not found`)
+      throw new errors.HttpStatusError(
+        404,
+        `Review with ID = ${reviewId} is not found`
+      );
     }
   } else {
-    review = response.rows[0]
+    review = response.rows[0];
   }
 
   // Fetch submission without review and review summations
-  const submission = yield SubmissionService._getSubmission(review.submissionId, false)
-  logger.info('Check User access before returning the review')
-  yield helper.checkReviewGetAccess(authUser, submission)
+  const submission = yield SubmissionService._getSubmission(
+    review.submissionId,
+    false
+  );
+  logger.info('Check User access before returning the review');
+  yield helper.checkReviewGetAccess(authUser, submission);
   // Return the review
-  return review
+  return review;
 }
 
 getReview.schema = {
   authUser: joi.object().required(),
-  reviewId: joi.string().uuid().required()
-}
+  reviewId: joi
+    .string()
+    .uuid()
+    .required()
+};
 
 /**
  * Function to list reviews from Elastic Search
  * @param {Object} query Query filters passed in HTTP request
  * @return {Object} Data fetched from ES
  */
-function * listReviews (query) {
-  return yield helper.fetchFromES(query, helper.camelize(table))
+function* listReviews(query) {
+  return yield helper.fetchFromES(query, helper.camelize(table));
 }
 
 const listReviewsQuerySchema = {
@@ -92,79 +102,106 @@ const listReviewsQuerySchema = {
   page: joi.id(),
   perPage: joi.pageSize(),
   orderBy: joi.sortOrder()
-}
+};
 
-listReviewsQuerySchema.sortBy = joi.string().valid(_.difference(
-  Object.keys(listReviewsQuerySchema),
-  ['page', 'perPage', 'orderBy']
-))
+listReviewsQuerySchema.sortBy = joi
+  .string()
+  .valid(
+    _.difference(Object.keys(listReviewsQuerySchema), [
+      'page',
+      'perPage',
+      'orderBy'
+    ])
+  );
 
 listReviews.schema = {
-  query: joi.object().keys(listReviewsQuerySchema).with('orderBy', 'sortBy')
-}
+  query: joi
+    .object()
+    .keys(listReviewsQuerySchema)
+    .with('orderBy', 'sortBy')
+};
 
 /**
  * Function to create review in database
  * @param {Object} entity Data to be inserted
  * @return {Promise}
  */
-function * createReview (authUser, entity) {
+function* createReview(authUser, entity) {
   // Check the validness of references using Helper function
-  yield HelperService._checkRef(entity)
+  yield HelperService._checkRef(entity);
 
-  const currDate = (new Date()).toISOString()
+  const currDate = new Date().toISOString();
 
-  const item = _.extend({
-    'id': uuid(),
-    'created': currDate,
-    'updated': currDate,
-    'createdBy': authUser.handle || authUser.sub,
-    'updatedBy': authUser.handle || authUser.sub,
-    'status': entity.status || 'completed'
-  }, entity)
+  const item = _.extend(
+    {
+      id: uuid(),
+      created: currDate,
+      updated: currDate,
+      createdBy: authUser.handle || authUser.sub,
+      updatedBy: authUser.handle || authUser.sub,
+      status: entity.status || 'completed'
+    },
+    entity
+  );
 
   // Prepare record to be inserted
   const record = {
     TableName: table,
     Item: item
-  }
+  };
 
-  yield dbhelper.insertRecord(record)
+  yield dbhelper.insertRecord(record);
 
   // Push Review created event to Bus API
   // Request body for Posting to Bus API
   const reqBody = {
-    'topic': events.submission.create,
-    'originator': originator,
-    'timestamp': currDate, // time when submission was created
+    topic: events.submission.create,
+    originator: originator,
+    timestamp: currDate, // time when submission was created
     'mime-type': mimeType,
-    'payload': _.extend({
-      'resource': helper.camelize(table)
-    }, item)
-  }
+    payload: _.extend(
+      {
+        resource: helper.camelize(table)
+      },
+      item
+    )
+  };
 
   // Post to Bus API using Client
-  yield helper.postToBusApi(reqBody)
+  yield helper.postToBusApi(reqBody);
 
   // Inserting records in DynamoDB doesn't return any response
   // Hence returning the same entity to be in compliance with Swagger
-  return item
+  return item;
 }
 
 createReview.schema = {
   authUser: joi.object().required(),
-  entity: joi.object().keys({
-    score: joi.score().required(),
-    typeId: joi.string().uuid().required(),
-    reviewerId: joi.alternatives()
-      .try(joi.id(), joi.string().uuid()).required()
-      .error(errors => ({message: '"reviewerId" must be a number or a string'})),
-    scoreCardId: joi.id().required(),
-    submissionId: joi.string().uuid().required(),
-    status: joi.reviewStatus(),
-    metadata: joi.object()
-  }).required()
-}
+  entity: joi
+    .object()
+    .keys({
+      score: joi.score().required(),
+      typeId: joi
+        .string()
+        .uuid()
+        .required(),
+      reviewerId: joi
+        .alternatives()
+        .try(joi.id(), joi.string().uuid())
+        .required()
+        .error(errors => ({
+          message: '"reviewerId" must be a number or a string'
+        })),
+      scoreCardId: joi.id().required(),
+      submissionId: joi
+        .string()
+        .uuid()
+        .required(),
+      status: joi.reviewStatus(),
+      metadata: joi.object()
+    })
+    .required()
+};
 
 /*
  * Function to update review in the database
@@ -174,22 +211,25 @@ createReview.schema = {
  * @param {Object} entity Data to be updated
  * @return {Promise}
  **/
-function * _updateReview (authUser, reviewId, entity) {
-  const exist = yield _getReview(reviewId)
+function* _updateReview(authUser, reviewId, entity) {
+  const exist = yield _getReview(reviewId);
   if (!exist) {
-    throw new errors.HttpStatusError(404, `Review with ID = ${reviewId} is not found`)
+    throw new errors.HttpStatusError(
+      404,
+      `Review with ID = ${reviewId} is not found`
+    );
   }
 
   // Check the validness of references using Helper function
-  yield HelperService._checkRef(entity)
+  yield HelperService._checkRef(entity);
 
-  const currDate = (new Date()).toISOString()
+  const currDate = new Date().toISOString();
 
   // Record used for updating in Database
   const record = {
     TableName: table,
     Key: {
-      'id': reviewId
+      id: reviewId
     },
     UpdateExpression: `set score = :s, scoreCardId = :sc, submissionId = :su,
                         typeId = :t, reviewerId = :r, #st = :st,
@@ -200,47 +240,55 @@ function * _updateReview (authUser, reviewId, entity) {
       ':su': entity.submissionId || exist.submissionId,
       ':t': entity.typeId || exist.typeId,
       ':r': entity.reviewerId || exist.reviewerId,
-      ':st': entity.status || exist.status,
+      ':st': entity.status || exist.status || 'completed',
       ':ua': currDate,
       ':ub': authUser.handle || authUser.sub
     },
     ExpressionAttributeNames: {
       '#st': 'status'
     }
-  }
+  };
 
   // If metadata exists, add it to the update expression
   if (entity.metadata || exist.metadata) {
-    record['UpdateExpression'] = record['UpdateExpression'] + `, metadata = :ma`
-    record['ExpressionAttributeValues'][':ma'] = _.merge({}, exist.metadata, entity.metadata)
+    record['UpdateExpression'] =
+      record['UpdateExpression'] + `, metadata = :ma`;
+    record['ExpressionAttributeValues'][':ma'] = _.merge(
+      {},
+      exist.metadata,
+      entity.metadata
+    );
   }
 
-  yield dbhelper.updateRecord(record)
+  yield dbhelper.updateRecord(record);
 
   // Push Review updated event to Bus API
   // Request body for Posting to Bus API
   const reqBody = {
-    'topic': events.submission.update,
-    'originator': originator,
-    'timestamp': currDate, // time when submission was updated
+    topic: events.submission.update,
+    originator: originator,
+    timestamp: currDate, // time when submission was updated
     'mime-type': mimeType,
-    'payload': _.extend({
-      'resource': helper.camelize(table),
-      'id': reviewId,
-      'updated': currDate,
-      'updatedBy': authUser.handle || authUser.sub
-    }, entity)
-  }
+    payload: _.extend(
+      {
+        resource: helper.camelize(table),
+        id: reviewId,
+        updated: currDate,
+        updatedBy: authUser.handle || authUser.sub
+      },
+      entity
+    )
+  };
 
   // Post to Bus API using Client
-  yield helper.postToBusApi(reqBody)
+  yield helper.postToBusApi(reqBody);
 
   // Updating records in DynamoDB doesn't return any response
   // Hence returning the response which will be in compliance with Swagger
   return _.extend(exist, entity, {
-    'updated': currDate,
-    'updatedBy': authUser.handle || authUser.sub
-  })
+    updated: currDate,
+    updatedBy: authUser.handle || authUser.sub
+  });
 }
 
 /**
@@ -250,23 +298,38 @@ function * _updateReview (authUser, reviewId, entity) {
  * @param {Object} entity Data to be updated
  * @return {Promise}
  */
-function * updateReview (authUser, reviewId, entity) {
-  return yield _updateReview(authUser, reviewId, entity)
+function* updateReview(authUser, reviewId, entity) {
+  return yield _updateReview(authUser, reviewId, entity);
 }
 
 updateReview.schema = {
   authUser: joi.object().required(),
-  reviewId: joi.string().uuid().required(),
-  entity: joi.object().keys({
-    score: joi.score().required(),
-    typeId: joi.string().uuid().required(),
-    reviewerId: joi.alternatives().try(joi.id(), joi.string().uuid()).required(),
-    scoreCardId: joi.id().required(),
-    submissionId: joi.string().uuid().required(),
-    status: joi.reviewStatus(),
-    metadata: joi.object()
-  }).required()
-}
+  reviewId: joi
+    .string()
+    .uuid()
+    .required(),
+  entity: joi
+    .object()
+    .keys({
+      score: joi.score().required(),
+      typeId: joi
+        .string()
+        .uuid()
+        .required(),
+      reviewerId: joi
+        .alternatives()
+        .try(joi.id(), joi.string().uuid())
+        .required(),
+      scoreCardId: joi.id().required(),
+      submissionId: joi
+        .string()
+        .uuid()
+        .required(),
+      status: joi.reviewStatus(),
+      metadata: joi.object()
+    })
+    .required()
+};
 
 /**
  * Function to patch review in database
@@ -275,13 +338,16 @@ updateReview.schema = {
  * @param {Object} entity Data to be patched
  * @return {Promise}
  */
-function * patchReview (authUser, reviewId, entity) {
-  return yield _updateReview(authUser, reviewId, entity)
+function* patchReview(authUser, reviewId, entity) {
+  return yield _updateReview(authUser, reviewId, entity);
 }
 
 patchReview.schema = {
   authUser: joi.object().required(),
-  reviewId: joi.string().uuid().required(),
+  reviewId: joi
+    .string()
+    .uuid()
+    .required(),
   entity: joi.object().keys({
     score: joi.score(),
     typeId: joi.string().uuid(),
@@ -291,49 +357,55 @@ patchReview.schema = {
     status: joi.reviewStatus(),
     metadata: joi.object()
   })
-}
+};
 
 /**
  * Function to delete review from database
  * @param {Number} reviewId reviewId which need to be deleted
  * @return {Promise}
  */
-function * deleteReview (reviewId) {
-  const exist = yield _getReview(reviewId)
+function* deleteReview(reviewId) {
+  const exist = yield _getReview(reviewId);
   if (!exist) {
-    throw new errors.HttpStatusError(404, `Review with ID = ${reviewId} is not found`)
+    throw new errors.HttpStatusError(
+      404,
+      `Review with ID = ${reviewId} is not found`
+    );
   }
 
   // Filter used to delete the record
   const filter = {
     TableName: table,
     Key: {
-      'id': reviewId
+      id: reviewId
     }
-  }
+  };
 
-  yield dbhelper.deleteRecord(filter)
+  yield dbhelper.deleteRecord(filter);
 
   // Push Review deleted event to Bus API
   // Request body for Posting to Bus API
   const reqBody = {
-    'topic': events.submission.delete,
-    'originator': originator,
-    'timestamp': (new Date()).toISOString(), // time when submission was deleted
+    topic: events.submission.delete,
+    originator: originator,
+    timestamp: new Date().toISOString(), // time when submission was deleted
     'mime-type': mimeType,
-    'payload': {
-      'resource': helper.camelize(table),
-      'id': reviewId
+    payload: {
+      resource: helper.camelize(table),
+      id: reviewId
     }
-  }
+  };
 
   // Post to Bus API using Client
-  yield helper.postToBusApi(reqBody)
+  yield helper.postToBusApi(reqBody);
 }
 
 deleteReview.schema = {
-  reviewId: joi.string().uuid().required()
-}
+  reviewId: joi
+    .string()
+    .uuid()
+    .required()
+};
 
 module.exports = {
   getReview,
@@ -342,4 +414,4 @@ module.exports = {
   updateReview,
   patchReview,
   deleteReview
-}
+};
