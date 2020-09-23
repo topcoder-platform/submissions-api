ed5a40df7298623ccfb248f2d2cbc673ffc01f14
James Cori <jamescori@gmail.com>
2020-09-23 11:56:31 -0400

    Removing health check

--- src/services/HealthCheckService.js
+++ src/services/HealthCheckService.js
@@ -2,23 +2,24 @@
  * Health Check Service
  */
 
-const errors = require('common-errors')
-const helper = require('../common/helper')
+// const errors = require('common-errors')
+// const helper = require('../common/helper')
 
 /**
  * Check if the elasticsearch connection is active
  */
 
 function * check () {
-  const esClient = helper.getEsClient()
+  // const esClient = helper.getEsClient()
 
-  try {
-    yield esClient.ping({
-      requestTimeout: 10000
-    })
-  } catch (e) {
-    throw new errors.HttpStatusError(503, 'Elasticsearch instance cannot be reached')
-  }
+  // try {
+  // yield esClient.ping({
+  //   requestTimeout: 10000
+  // })
+
+  // } catch (e) {
+  //   throw new errors.HttpStatusError(503, 'Elasticsearch instance cannot be reached')
+  // }
 
   return {
     checksRun: 1
