b1e2fafb946e4929450df4b85fc6c41e164e9638
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2021-03-02 20:31:08 +0530

    typo

--- src/common/helper.js
+++ src/common/helper.js
@@ -376,11 +376,11 @@ function * getSubmissionPhaseId (challengeId) {
     const submissionPh = _.filter(phases, { name: 'Submission', isOpen: true })
     const finalFixPh = _.filter(phases, { name: 'Final Fix', isOpen: true })
     if (checkPoint.length !== 0) {
-      phaseId = _.get('checkPoint[0]', 'phaseId', null)
+      phaseId = _.get(checkPoint, "[0].phaseId", null)
     } else if (submissionPh.length !== 0) {
-      phaseId = _.get('submissionPh[0]', 'phaseId', null)
+      phaseId = _.get(submissionPh, "[0].phaseId", null)
     } else if (finalFixPh.length !== 0) {
-      phaseId = _.get('finalFixPh[0]', 'phaseId', null)
+      phaseId = _.get(finalFixPh, "[0].phaseId", null)
     }
   }
   return phaseId
